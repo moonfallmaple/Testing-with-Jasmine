4077cacd707c9ca4639f0c6caf5b5561e9729bf5
moonfalljade <moonfalljade@hotmail.com>
2018-12-24 11:38:40 +0800

    first commit

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,36 @@
+describe('Address Book',function(){
+    it('should be able to add a contact',function(){
+        let addressBook = new AddressBook(),
+            thisContact = new Contact();
+
+        addressBook.addContact(thisContact);
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+
+    it('should be able to delete a contact',function(){
+        let addressBook = new AddressBook(),
+            thisContact = new Contact();
+
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
+});
+
+describe('Async Address Book',function(){
+    let addressBook = new AddressBook();
+
+    beforeEach(function(done){
+        addressBook.getInitialContacts(function(){
+                done();
+        });
+    });
+    
+    it('it should grab initial contacts',function(done){
+        expect(addressBook.initialComplete).toBe(true);
+        done();
+    });
+
+  
+});
---
+++ src/Contact.js
@@ -0,0 +1,4 @@
+// needs to make an PI call to a server to retrieve a list of initial contacts to put back into our application
+function Contact() {
+    // this.contacts=[];
+}
